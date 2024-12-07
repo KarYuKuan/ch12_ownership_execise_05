fb16a015ae3ee8e78b58a9506a34d06ad3586036
Samurai_Chia <jyjzone@163.com>
2024-12-07 17:22:56 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello this is rust ");
+    let x = 66;
+    take_ownership(s);
+
+    makes_copy(x);
+
+    println!("the values of s is :{x}");
+    //下面一行無法編譯通過，因為S已經被傳入take_ownership方法且作用域完畢後被釋放了
+    //println!("the values of s is :{s}");
+
+}
+
+fn take_ownership(some_string:String ){
+    println!("the value of some_thing is : {some_string}");
+}
+
+fn makes_copy(some_interger:i32){
+    println!("the value of some_thing is : {some_interger}");
 }
